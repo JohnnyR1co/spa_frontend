c8708fd7f168de302dd6b23830e04048c94eba95
Johnny Rico <rubydev@qq.com>
2016-10-14 20:41:47 +1000

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://127.0.0.1:4000',
+    'webpack/hot/only-dev-server',
+    './src/index.js'
+  ],
+  output: {
+    path: '/Users/Apple/React/spa_frontend/build',
+    filename: 'index.bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loaders: ['babel-loader', 'eslint-loader'],
+      include: path.join(__dirname, 'src')
+    }]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
