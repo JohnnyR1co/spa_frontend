8d31b0cbed42e59f002718b28f7406b7e8a1373f
Johnny Rico <rubydev@qq.com>
2016-10-19 20:38:09 +1000

    change Post.js, add news

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Post from './Post';
 import PostForm from './PostForm'
+import posts from './posts'
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <Post />
+        <Post posts = {posts} />
         <PostForm />
       </div>
     );
